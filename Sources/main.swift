de686cf750f608f3279aa068c4b50a30f1a9509b
masarusanjp <masarusanplusplus@gmail.com>
2017-03-04 12:43:23 +0900

    implemented as cli

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,3 +7,9 @@
 //
 
 import Foundation
+
+let data = FileHandle.standardInput.availableData
+let convertedData = String(data: data, encoding: .utf8)?.swiftringify.data(using: .utf8)
+if let convertedData = convertedData {
+    FileHandle.standardOutput.write(convertedData)
+}
